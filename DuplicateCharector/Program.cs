ab61896b3e2d508cc1a9f04442aa4ad23e7128f4
dinuv <dinu.vnair@gmail.com>
2023-08-08 06:38:44 +0530

    changes

--- DuplicateCharector/Program.cs
+++ DuplicateCharector/Program.cs
@@ -1,37 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 //Console.WriteLine("Hello, World!");
 
 StringFunctions stringFunctions = new StringFunctions();
 
-stringFunctions.UniquwChar("Hello, World!");
+stringFunctions.ReverseWords("Hello, World!");
 
 public class StringFunctions
 {
+    public void ReverseWords(string originalString)
+    {
+        StringBuilder sb = new StringBuilder(); 
+        string[] strings = originalString.Split(" ");
+        foreach (string s in strings)
+        {
+            string result = "";
+            for(int i=s.Length-1; i>=0; i--)
+            {
+                result += s[i];
+            }
+            sb.Append(result);
+            sb.Append(" ");
+        }
+        Console.WriteLine(sb.ToString());
+    }
+    public void Reverse(string s)
+    {
+        char[] chars = s.ToCharArray();
+        Array.Reverse(chars);
+        string result = new String(chars);
+
+        //string result = "";
+        //for(int i=s.Length-1; i>=0;i--)
+        //{
+        //    result += s[i];
+        //}
+        Console.WriteLine(result);
+    }
     public void UniquwChar(string s)
     {
-        string uc =new string(s.Distinct().ToArray());
-        Console.WriteLine(uc);
-        //StringBuilder result = new StringBuilder();
-        //StringBuilder   uc = new StringBuilder();
-        //foreach (char c in s)
-        //{
-        //    if (result.ToString().IndexOf(c.ToString().ToLower()) == -1)
-        //    {
-        //        result.Append(c);
-        //    }
-        //}
-        //Console.WriteLine(result);
-        //var ch=new List<char>();
 
-        //foreach (char c in s)
-        //{
-        //    if(s.IndexOf(c) == s.LastIndexOf(c))
-        //    {  ch.Add(c); }
-        //}
-        //Console.WriteLine
     }
     public StringBuilder FindDuplicateChar(string Text)
     {
@@ -52,14 +63,15 @@ public class StringFunctions
 
     public void FindDuplicateChar1(string Text)
     {
-        var chars=new List<char>(); 
-        foreach(char c in Text) { 
+        var chars = new List<char>();
+        foreach (char c in Text)
+        {
 
-            if(Text.IndexOf(c)!=Text.LastIndexOf(c) && !chars.Contains(c))
+            if (Text.IndexOf(c) != Text.LastIndexOf(c) && !chars.Contains(c))
             {
                 chars.Add(c);
             }
-        
+
         }
         Console.WriteLine(string.Join(",", chars));
 
@@ -72,6 +84,6 @@ public class StringFunctions
             Console.WriteLine(group.Key);
         }
     }
-   
+
 
 }
